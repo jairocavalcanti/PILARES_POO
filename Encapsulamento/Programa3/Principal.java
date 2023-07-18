931c52ba3738b1b88c982df2782060750d4838c0
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-18 01:05:14 -0300

    Inserindo pratico encapsulamento

--- Encapsulamento/Programa3/Principal.java
+++ Encapsulamento/Programa3/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
     public static void main(String[] args) {
        PainelTesla pan = new PainelTesla();
 
+       System.out.println("-------------------------------------");
        pan.abrir_portas_dianteiras();
        pan.abrir_portas_dianteiras();
        pan.fechar_portas_dianteiras();
