32253d93287a48e2acd3a4741134eea68327bdb3
Jairo <jairoregiscavalcanti@gmail.com>
2024-02-27 16:50:50 -0300

    Programa_5_atualizado_#2

--- Encapsulamento/Programa5/ContaBancaria.java
+++ Encapsulamento/Programa5/ContaBancaria.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class ContaBancaria {
 
-    private Integer numero_da_conta;
-    private static Double saldo = 10000.00;
-    private String nome;
+    private Integer numero_da_conta  = 0;
+    private Double saldo = 0.0;
+    private String nome = "";
     private String tipo_de_conta;
    
 
@@ -15,8 +15,9 @@ public class ContaBancaria {
 
     }
 
-    public ContaBancaria(Integer numero_da_conta, String nome, String tipo_de_conta) {
+    public ContaBancaria(Integer numero_da_conta, Double saldo, String nome, String tipo_de_conta) {
         this.numero_da_conta = numero_da_conta;
+        this.saldo = saldo;
         this.nome = nome;
         this.tipo_de_conta = tipo_de_conta;
     }
@@ -32,6 +33,10 @@ public class ContaBancaria {
 
     public Double getSaldo() {
         return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
     }
 
     public String getNome() {
@@ -52,48 +57,86 @@ public class ContaBancaria {
 
 
     public Integer numero_de_conta(Scanner scanner){
+        
+        while(numero_da_conta < 3){
         System.out.printf("Insira o numero da conta: ");
         int novo_numero_conta = scanner.nextInt();
 
-        setNumero_da_conta(novo_numero_conta);
-        
-        System.out.printf("Numero de conta inserido - #");
+        if(novo_numero_conta > 3){
+           setNumero_da_conta(novo_numero_conta);
+           System.out.printf("Numero de conta inserido - #");
+           break;
+        }else{
+           System.out.println("Numero de conta inserido possui menos de 3 algarismos! ");
+          }
+        }
         
         return getNumero_da_conta();
     
     }
 
     public String verificar_saldo(){
+       
+        if(saldo > 0){
+
         DecimalFormat df = new DecimalFormat("R$ ##.###");
         System.out.printf("Saldo disponivel da conta - #" + getNumero_da_conta() + " - ");
        
         String saldo_formatado =  df.format(saldo);
 
         return saldo_formatado;
+        }else{
+        return "Saldo indisponivel ! ";
+       
+        }
     }
     
     public String inserir_nome(Scanner scanner){
+        
+        while (nome.length() < 5) {
         System.out.printf("Insira o nome do titular da conta: ");
         String nome = scanner.next();
       
-        setNome(nome);
- 
+        if(nome.length() > 5){
+           System.out.println("Nome de titular inserido com sucesso ! \n");
+           setNome(nome);
+           break;
+        }else{
+            System.out.println("!! Nome inserido tem menos de 5 caracteres !!");
+        }
+    }
         System.out.printf("Titular da conta de numero #" + getNumero_da_conta() + "-" + " ");
         return getNome();
-     }
+    }
 
-     public void sacar_valor(Double valor){
-         saldo =- valor;
-         System.out.println("Saque de " + valor + " realizado !");          
-     }
+    public void sacar_valor(Double valor){
+        if(saldo > valor){
+            saldo -= valor;
+            System.out.println("Saque de R$: " + valor + " realizado !");   
+        }else{
+            System.out.println("Saldo insuficiente para saque! ");
+        }         
+    }
+
+    public void depositar_valor(Double valor){
+        saldo += valor;
+        System.out.println("Depósito na quantia de R$: " + valor + " realizado !");
+    }
+
 
      @Override
      public String toString() {
         StringBuilder sb = new StringBuilder();
 
+        sb.append("-- INFORMAÇÕES GERAIS -- \n");
 
+        sb.append("Nome: ").append(nome).append("\n");
+        sb.append("Numero da conta: ").append(numero_da_conta).append("\n");
+        sb.append("Tipo de conta: ").append(tipo_de_conta).append("\n");
+        sb.append("Saldo: ").append(saldo);
+       
         return sb.toString();
-     }
+    }
 
 
 }
--- Encapsulamento/Programa5/Principal.java
+++ Encapsulamento/Programa5/Principal.java
@@ -7,12 +7,56 @@ public class Principal {
       Scanner scanner = new Scanner(System.in);
       ContaBancaria cb = new ContaBancaria();
 
-      System.out.println(cb.numero_de_conta(scanner));
-      System.out.println("--");
-      System.out.println(cb.inserir_nome(scanner));
-      System.out.println("--");
-      System.out.println(cb.verificar_saldo());
+      System.out.println("Insira a opção de conta que deseja criar: PF/PJ");
+      String opcao = scanner.next();
 
+      if(opcao.toLowerCase().equals("pf")){
+
+        cb.setTipo_de_conta("PF");
+
+        System.out.println(cb.numero_de_conta(scanner));
+        System.out.println("--");
+        System.out.println(cb.inserir_nome(scanner));
+       
+        String init = "m";
+
+        while (init.equals("m")) {
+          
+          System.out.println("Insira a operação que deseja realizar: ");
+          int escolha = scanner.nextInt();
+
+          switch (escolha) {
+           
+            case 1:
+            System.out.println("Insira a quantidade que deseja depositar: ");
+            Double deposito = scanner.nextDouble();
+            cb.depositar_valor(deposito);
+            break;
+          
+            case 2:
+            System.out.println("Insira a quantidade que deseja sacar: ");
+            Double saque = scanner.nextDouble();
+            cb.sacar_valor(saque);
+            break;
+
+            case 3: 
+            System.out.println(cb.verificar_saldo());
+            break;
+
+            case 4:
+            System.out.println(cb.toString());
+            break;
+
+            default:
+            System.out.println("!! Insira uma oção válida !!");
+            break;
+          }
+
+        }
+
+      }
+     
+      
       scanner.close();
   }
 
